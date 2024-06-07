e9447225d63a6638144cb15ad874221c715b9fcc
galangriarulhaq <galangrz337@gmail.com>
2024-06-07 22:43:52 +0700

    Learn to add animation simple

--- app/src/main/java/com/example/jetpack_compose_beginner/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_beginner/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jetpack_compose_beginner
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.Surface
 import androidx.activity.ComponentActivity
@@ -14,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -58,23 +61,138 @@ fun MessageCard(msg: Message) {
         Column {
             Text(
                 text = msg.author,
-                color = MaterialTheme.colorScheme.secondary
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Text(text = msg.body)
+            Surface(shape = MaterialTheme.shapes.medium,
+                shadowElevation = 2.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
         }
     }
 
+}
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Lexi",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Lexi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Lexi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Lexi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Lexi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Lexi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Lexi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Lexi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Lexi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Lexi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message -> 
+            MessageCard(message)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewMessageCard() {
+fun PreviewConversation() {
     Jetpack_Compose_BeginnerTheme {
-        Surface {
-            MessageCard(
-                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-            )
-        }
+        Conversation(SampleData.conversationSample)
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun PreviewMessageCard() {
+//    Jetpack_Compose_BeginnerTheme {
+//        Surface {
+//            MessageCard(
+//                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+//            )
+//        }
+//    }
+//}
+
+
+// Preview 2 mode sekaligus
+//@Preview("Ligt Mode")
+//@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
+//@Composable
+//fun PreviewMessageCard() {
+//    Jetpack_Compose_BeginnerTheme {
+//        Surface {
+//            MessageCard(
+//                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+//            )
+//        }
+//    }
+//}
